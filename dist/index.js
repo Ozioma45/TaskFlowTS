8670500518254556099658f39f2257ddd221795c
Ozioma45 <oziomaegole@gmail.com>
2025-02-24 16:17:59 +0100

    create TodoList Class, implement the addTodo method, completeTodomethod, removeTodo method

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,28 @@
 "use strict";
+class TodoList {
+    constructor() {
+        this.todos = [];
+        this.nextId = 1;
+    }
+    addTodo(task, dueDate) {
+        const newTodo = {
+            id: this.nextId++,
+            task,
+            completed: false,
+            dueDate,
+        };
+        this.todos.push(newTodo);
+    }
+    completeTodo(id) {
+        const todo = this.todos.find((todo) => todo.id === id);
+        if (todo) {
+            todo.completed = true;
+        }
+        else {
+            console.error(`Todo with ID ${id} not found.`);
+        }
+    }
+    removeTodo(id) {
+        this.todos = this.todos.filter((todo) => todo.id !== id);
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,31 @@ interface TodoItem {
   completed: boolean;
   dueDate: Date;
 }
+
+class TodoList {
+  private todos: TodoItem[] = [];
+  private nextId: number = 1;
+
+  addTodo(task: string, dueDate: Date): void {
+    const newTodo: TodoItem = {
+      id: this.nextId++,
+      task,
+      completed: false,
+      dueDate,
+    };
+    this.todos.push(newTodo);
+  }
+
+  completeTodo(id: number): void {
+    const todo = this.todos.find((todo) => todo.id === id);
+    if (todo) {
+      todo.completed = true;
+    } else {
+      console.error(`Todo with ID ${id} not found.`);
+    }
+  }
+
+  removeTodo(id: number): void {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  }
+}
